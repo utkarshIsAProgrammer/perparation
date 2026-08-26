763ac7562d89a82c18dddd6a08a030d6bfc096a9
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-08-26 23:56:46 +0530

    error handling

--- golang/019_error_handling/main.go
+++ golang/019_error_handling/main.go
@@ -1,6 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func run() {}
+
+func parse(s string) (int, error) {
+	value, error := strconv.Atoi(s)
+	if error != nil {
+		return 0, fmt.Errorf("Value must be a number!")
+	}
+
+	if value < 1 || value > 5 {
+		return 0, fmt.Errorf("Value must be in between 1 and 5")
+	}
+
+	return value, nil
+}
 
 func main() {
 	fmt.Println()
